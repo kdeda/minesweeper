af6761646ed1c4eea2e82208f7285fd7c49d6268
jdeda <jldeda00@gmail.com>
2022-05-08 14:06:43 -0400

    gridcell to tca

--- Shared/AppFeature/GridFeature/GridState.swift
+++ Shared/AppFeature/GridFeature/GridState.swift
@@ -5,104 +5,117 @@ struct GridState: Equatable {
     static var cellSize: CGFloat = 24.0
     var fps = 20
     var cells: Matrix<GridCell>
-    
+
     init(rows: Int = 3, cols: Int = 4) {
         self.cells = Matrix(rows, cols) { row, column in
             GridCell(row: row, column: column)
         }
-        
-        // example of a map on Matrix
-        //      let foo = self.cells
-        //          .map { row, column in
-        //              [row, column]
-        //          }
-        //          .map { row, column in
-        //              GridCell(row: row, column: column)
-        //          }
     }
 }
 
 enum GridAction {
-    case reset
-    case resetCells
-    case flipCorners
-    case clockWise
-    case updatedCell(GridCell)
+  case reset
+  case resetCells
+  case flipCorners
+  case clockWise
+  case updatedCell(GridCellState)
+  case cell(id: GridCellState.ID, action: GridCellAction)
 }
 
 struct GridEnvironement {
-    struct FlipCornersID: Hashable {}
-    struct ClockWiseID: Hashable {}
-    
-    var mainQueue: AnySchedulerOf<DispatchQueue>
-    var uuid: () -> UUID
-    
-    func reset(state: inout GridState) {
-        state.cells = state.cells.map { row, column in
-            GridCell(row: row, column: column)
+  struct FlipCornersID: Hashable {}
+  struct ClockWiseID: Hashable {}
+
+  var mainQueue: AnySchedulerOf<DispatchQueue>
+  var uuid: () -> UUID
+
+  func reset(state: inout GridState) {
+    state.cells = try! Matrix(
+      (0..<state.cells.rows).map { row in
+        (0..<state.cells.cols).map { col in
+          GridCellState(row: row, column: col)
         }
+      }
+    )
+  }
+
+  // Turn all cells gray, start at(0,0), turn corner cells yellow in coordinated fashion.
+  func flipCorners(state: inout GridState, fps: Int = 6) -> Effect<GridCellState, Never> {
+    reset(state: &state)
+    let publisher = state.cells.getEntries()
+      .flatMap { $0 }
+      .compactMap { cell -> GridCellState? in
+        let cornerRow = cell.row == 0 || cell.row == state.cells.rows - 1
+        let cornerColumn = cell.column == 0 || cell.column == state.cells.cols - 1
+        return cornerRow && cornerColumn ? cell : nil
+      }
+      .publisher
+      .zip(Effect.timer(id: FlipCornersID(), every: .seconds(1.0 / Double(fps)), on: mainQueue))
+      .map { cell, _ -> GridCellState in
+        GridCellState(row: cell.row, column: cell.column, rotation: cell.rotation + 90.0, color: .yellow)
+      }
+      .eraseToAnyPublisher()
+      .eraseToEffect()
+    return publisher
+  }
+
+  func cancelFlipCorners() -> Effect<GridAction, Never> { Effect.cancel(id: FlipCornersID()) }
+
+  // Turn all cells gray, start at(0,0), move in clockwise fashion, turn visited cells yellow.
+  func clockWise(state: inout GridState, fps: Int = 6) -> Effect<GridCellState, Never> {
+
+  // Prepare to move.
+  var currentMove = Move(
+      minRows: 0, maxRows: state.cells.rows,
+      minColumns: 0, maxColumns: state.cells.cols,
+      row: 0, column: 0, direction: .right
+  )
+  var orderedCells = [state.cells.at(currentMove.row,currentMove.column)]
+  var working = true
+
+  // Reset grid.
+  reset(state: &state)
+
+  // Move until complete.
+  repeat {
+      let newMove = currentMove.nextClockWiseMove
+      if currentMove == newMove { working = false }
+      else {
+          orderedCells.append(state.cells.at(newMove.row, newMove.column))
+          currentMove = newMove
+      }
+  } while working
+
+    // Animate.
+    let publisher = orderedCells
+      .publisher
+      .zip(Effect.timer(id: ClockWiseID(), every: .seconds(1.0 / Double(fps)), on: mainQueue))
+      .map { cell, _ -> GridCellState in
+        GridCellState(row: cell.row, column: cell.column, rotation: cell.rotation + 90.0, color: .yellow)
+      }
+      .eraseToAnyPublisher()
+      .eraseToEffect()
+    return publisher
+  }
+
+  func cancelClockWise() -> Effect<GridAction, Never> { Effect.cancel(id: ClockWiseID()) }
+}
+
+extension GridState {
+  static let reducer = Reducer<GridState, GridAction, GridEnvironement>.combine(
+    Reducer { state, action, environment in
+      switch action {
+      case .resetCells:
+        environment.reset(state: &state)
+        return .none
+      case let .updatedCell(cell):
+        state.cells.replaceAt(row: cell.row, col: cell.column, entry: cell)
+        return .none
+      case .cell:
+        return .none
+      }
     }
-    
-    // Turn all cells gray, start at(0,0), turn corner cells yellow in coordinated fashion.
-    func flipCorners(state: inout GridState, fps: Int = 6) -> Effect<GridCell, Never> {
-        reset(state: &state)
-        let publisher = state.cells.entries
-            .flatMap { $0 }
-            .compactMap { cell -> GridCell? in
-                let cornerRow = cell.row == 0 || cell.row == state.cells.rows - 1
-                let cornerColumn = cell.column == 0 || cell.column == state.cells.cols - 1
-                return cornerRow && cornerColumn ? cell : nil
-            }
-            .publisher
-            .zip(Effect.timer(id: FlipCornersID(), every: .seconds(1.0 / Double(fps)), on: mainQueue))
-            .map { cell, _ -> GridCell in
-                GridCell(row: cell.row, column: cell.column, rotation: cell.rotation + 90.0, color: .yellow)
-            }
-            .eraseToAnyPublisher()
-            .eraseToEffect()
-        return publisher
-    }
-    
-    func cancelFlipCorners() -> Effect<GridAction, Never> { Effect.cancel(id: FlipCornersID()) }
-    
-    // Turn all cells gray, start at(0,0), move in clockwise fashion, turn visited cells yellow.
-    func clockWise(state: inout GridState, fps: Int = 6) -> Effect<GridCell, Never> {
-        
-        // Prepare to move.
-        var currentMove = Move(
-            minRows: 0, maxRows: state.cells.rows,
-            minColumns: 0, maxColumns: state.cells.cols,
-            row: 0, column: 0, direction: .right
-        )
-        var orderedCells = [state.cells.at(currentMove.row,currentMove.column)]
-        var working = true
-        
-        // Reset grid.
-        reset(state: &state)
-        
-        // Move until complete.
-        repeat {
-            let newMove = currentMove.nextClockWiseMove
-            if currentMove == newMove { working = false }
-            else {
-                orderedCells.append(state.cells.at(newMove.row, newMove.column))
-                currentMove = newMove
-            }
-        } while working
-        
-        
-        // Animate.
-        let publisher = orderedCells
-            .publisher
-            .zip(Effect.timer(id: ClockWiseID(), every: .seconds(1.0 / Double(fps)), on: mainQueue))
-            .map { cell, _ -> GridCell in
-                GridCell(row: cell.row, column: cell.column, rotation: cell.rotation + 90.0, color: .yellow)
-            }
-            .eraseToAnyPublisher()
-            .eraseToEffect()
-        return publisher
-    }
-    
+
     func cancelClockWise() -> Effect<GridAction, Never> { Effect.cancel(id: ClockWiseID()) }
 }
 
@@ -113,14 +126,14 @@ extension GridState {
             case .resetCells:
                 environment.reset(state: &state)
                 return .none
-                
+
             case .reset:
                 return .merge(
                     environment.cancelFlipCorners(),
                     environment.cancelClockWise(),
                     Effect(value: .resetCells)
                 )
-                
+
             case .flipCorners:
                 return .merge(
                     environment.cancelFlipCorners(),
@@ -129,7 +142,7 @@ extension GridState {
                         .flipCorners(state: &state, fps: state.fps)
                         .map(GridAction.updatedCell)
                 )
-                
+
             case .clockWise:
                 return .merge(
                     environment.cancelFlipCorners(),
@@ -137,7 +150,7 @@ extension GridState {
                     environment.clockWise(state: &state, fps: state.fps)
                         .map(GridAction.updatedCell)
                 )
-                
+
             case let .updatedCell(cell):
                 state.cells.replaceAt(row: cell.row, col: cell.column, entry: cell)
                 return .none
@@ -155,7 +168,7 @@ extension GridState {
             uuid: UUID.init
         )
     )
-    
+
     static let mockStore = Store<GridState, GridAction>(
         initialState: .init(),
         reducer: reducer,
--- Shared/AppFeature/GridFeature/GridView.swift
+++ Shared/AppFeature/GridFeature/GridView.swift
@@ -2,64 +2,39 @@ import SwiftUI
 import ComposableArchitecture
 
 struct GridView: View {
-    let store: Store<GridState, GridAction>
-    
-    var body: some View {
-        WithViewStore(store) { viewStore in
-            VStack(spacing: 1) {
-                VStack(spacing: 1) {
-                    Text("\(viewStore.cells.rows) by \(viewStore.cells.cols)")
-                        .font(.caption2)
-                        .fontWeight(.semibold)
-                        .padding(.bottom, 6)
-                    
-                    ForEach(0 ..< viewStore.cells.rows, id: \.self) { row in
-                        HStack(spacing: 1) {
-                            ForEach(0 ..< viewStore.cells.cols, id: \.self) { column in
-                                GridCellView(cell: viewStore.cells.at(row, column))
-                            }
-                        }
-                    }
-                    
-                    HStack(spacing: 2) {
-                        Text(Image(systemName: "checkmark.circle.fill"))
-                            .font(.caption)
-                            .fontWeight(.bold)
-                    }
-                    .padding(.top, 6)
-                }
-            }
-            .drawingGroup()
-            .padding(.all, 6)
-            .border(Color.gray)
-        }
-    }
-}
+  let store: Store<GridState, GridAction>
 
-struct GridCellView: View {
-    var cell: GridCell
-    @State private var rotation: CGFloat = 0.0
-    @State private var cellSize: CGFloat = GridCell.viewSize
-    @State private var cornerRadius: CGFloat = 0.0
-    
-    init(cell: GridCell) {
-        self.cell = cell
-        self.rotation = cell.rotation // Start with currentt cell rotation.
-    }
-    
-    var body: some View {
-        return ZStack {
-            Rectangle()
-                .fill(Color.clear)
-                .frame(width: cellSize, height: cellSize)
-                .background(cell.color)
-                .rotationEffect(.degrees(rotation))
-            Text("\(cell.row),\(cell.column)")
-                .font(.caption2)
-                .foregroundColor(.white)
+  var body: some View {
+    WithViewStore(store) { viewStore in
+      VStack(spacing: 1) {
+        Text("\(viewStore.cells.rows) by \(viewStore.cells.cols)")
+          .font(.caption2)
+          .fontWeight(.semibold)
+          .padding(.bottom, 6)
+
+        // TODO: Figure this out. Need identified array of?
+        ForEach(0 ..< viewStore.cells.rows, id: \.self) { row in
+          HStack(spacing: 1) {
+            ForEachStore(self.store.scope(
+              state: \.cells.entries[row],
+              action: GridAction.cell
+            )) { childStore in
+              GridCellView(store: childStore)
+            }
+          }
         }
-        .frame(width: GridCell.viewSize, height: GridCell.viewSize)
+
+        HStack(spacing: 2) {
+          Text(Image(systemName: "checkmark.circle.fill"))
+            .font(.caption)
+            .fontWeight(.bold)
+        }
+        .padding(.top, 6)
+      }
+      .drawingGroup() // TODO: Why use this?
+      .padding(.all, 6)
     }
+  }
 }
 
 struct GridView_Previews: PreviewProvider {
@@ -68,4 +43,3 @@ struct GridView_Previews: PreviewProvider {
             .environment(\.colorScheme, .light)
     }
 }
-
